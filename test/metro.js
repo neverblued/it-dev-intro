5725a9f6bbb80d0879e606672794df2244809fa8
Dmytro Pinskyi <neverblued@gmail.com>
2014-08-06 23:37:36 +0300

    update

--- test/metro.js
+++ test/metro.js
@@ -1,37 +1,37 @@
-var assert = require('assert');
 var metro = require('../model/metro');
-var test = {};
+var tourniquet, coin, person = 'Василий Пупкин';
+require('should');
 
 describe('metro', function(){
 	
 	it('should be ready', function(){
-		assert.equal(typeof metro.bank, 'object');
-		assert.equal(typeof metro.coin, 'function');
-		assert.equal(typeof metro.tourniquet, 'function');
+		metro.bank.should.be.type('object');
+		metro.coin.should.be.type('function');
+		metro.tourniquet.should.be.type('function');
 	});
 	
 	it('should produce coins', function(){
-		test.coin = new metro.coin;
-		assert.equal(metro.bank.check(test.coin), true);
+		coin = new metro.coin;
+		metro.bank.check(coin).should.be.true;
 	});
 	
 	describe('tourniquet', function(){
 		
 		it('should be ready', function(){
-			test.tourniquet = new metro.tourniquet;
-			assert.equal(test.tourniquet.access(), false);
+			tourniquet = new metro.tourniquet;
+			tourniquet.access().should.be.false;
 		});
 		
 		it('should consume coins and grant people to enter', function(){
-			test.tourniquet.feed(test.coin);
-			assert.equal(test.tourniquet.access(), true);
-			assert.equal(metro.bank.check(test.coin), false);
+			tourniquet.feed(coin);
+			tourniquet.access().should.be.true;
+			metro.bank.check(coin).should.be.false;
 		});
 
 		it('should let people go through', function(){
-			test.person = 'Василий Пупкин';
-			test.tourniquet.pass(test.person);
-			assert.equal(test.tourniquet.access(), false);
+			person = 'Василий Пупкин';
+			tourniquet.pass(person);
+			tourniquet.access().should.be.false;
 		});
 	});
 });

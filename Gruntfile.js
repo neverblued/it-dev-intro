c47f6283913927e5e00ed3bcbd66088438988e95
Dmytro Pinskyi <neverblued@gmail.com>
2014-08-06 22:16:32 +0300

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,14 @@
 module.exports = function(grunt){
-.	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+	
+	grunt.initConfig({
+//		pkg: grunt.file.readJSON('package.json'),
+		
 		jscs: {
 			src: "*/*.js",
 			options: {
-				config: ".jscsrc",
-				requireCurlyBraces: [ "if" ]
+				config: ".jscsrc"
 			}
 		}
 	});
 
-
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-
-	grunt.registerTask('default', ['concat', 'less', 'uglify']);
 };

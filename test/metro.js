dbb688803a897e8f92d2f8b992847de7f4934ca1
Dmytro Pinskyi <neverblued@gmail.com>
2014-08-07 17:03:42 +0300

    add tests

--- test/metro.js
+++ test/metro.js
@@ -1,46 +1,60 @@
 var metro = require('../model/metro');
 
-var tourniquet;
-var coin;
-var fake = "фальшивка";
-var people = ['Василий Пупкин', 'Надежда Крупская', 'Вера Брежнева'];
-var next = function(){
-	return people.shift();
-};
+var nextPerson = (function(){
+	var people = ['Василий Пупкин', 'Надежда Крупская', 'Вера Брежнева'];
+	return function(){
+		return people.shift();
+	};
+})();
 
 require('should');
 
-describe('Метро', function(){
-	it('Проверка сущностей', function(){
+describe('Модель', function(){
+	it('описывает сущности', function(){
 		metro.bank.should.be.type('object');
 		metro.coin.should.be.type('function');
 		metro.tourniquet.should.be.type('function');
 	});
+	it('описывает протокол жетонов', function(){
+		metro.bank.check.should.be.type('function');
+		metro.bank.activate.should.be.type('function');
+	});
+	it('описывает методы турникета', function(){
+		['feed', 'pass', 'consume', 'reject', 'grant', 'let', 'access'].forEach(function(method){
+			metro.tourniquet.prototype[method].should.be.type('function');
+		});
+	});
 });
-	
+
 describe('Жетоны', function(){
 	
+	it('проверяются', function(){
+		metro.bank.check(undefined).should.be.false;
+		metro.bank.check('фальшивка').should.be.false;
+	});
+	
+	var coin = new metro.coin;
+	
 	it('производятся', function(){
-		coin = new metro.coin;
 		metro.bank.check(coin).should.be.true;
 	});
 	
-	it('проверяются', function(){
-		coin = undefined;
+	it('используются', function(){
+		coin.use();
 		metro.bank.check(coin).should.be.false;
 	});
 });
-	
+
 describe('Турникет', function(){
+	var tourniquet = new metro.tourniquet;
 		
 	it('готов', function(){
-		tourniquet = new metro.tourniquet;
 		tourniquet.access().should.be.false;
 	});
 
-	it('принимает жетоны', function(){
+	it('принимает два жетона', function(){
 		var processCoin = function(){
-			coin = new metro.coin;
+			var coin = new metro.coin;
 			(function(){
 				tourniquet.feed(coin);
 			}).should.not.throw();
@@ -51,25 +65,17 @@ describe('Турникет', function(){
 		processCoin();
 	});
 
-	it('не принимает использованнные жетоны', function(){
-		metro.bank.check(coin).should.be.false;
+	it('не принимает фальшивку', function(){
 		(function(){
-			tourniquet.feed(coin);
+			tourniquet.feed('фальшивка');
 		}).should.throw(metro.message.injury);
 	});
 
-	it('не принимает фальшивые жетоны', function(){
-		metro.bank.check(fake).should.be.false;
-		(function(){
-			tourniquet.feed(fake);
-		}).should.throw(metro.message.injury);
-	});
-
-	it('пропускает двоих', function(){
+	it('пропускает двоих людей за два жетона', function(){
 		var letPerson = function(){
 			tourniquet.access().should.be.true;
 			(function(){
-				tourniquet.pass(next());
+				tourniquet.pass(nextPerson());
 			}).should.not.throw();
 		};
 		letPerson();
@@ -79,7 +85,7 @@ describe('Турникет', function(){
 	it('не пропускает зайца', function(){
 		tourniquet.access().should.be.false;
 		(function(){
-			tourniquet.pass(next());
+			tourniquet.pass(nextPerson());
 		}).should.throw(metro.message.denial);
 	});
 });
